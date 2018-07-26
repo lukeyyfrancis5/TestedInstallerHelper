10176d1f72ca2c22c6dead9ec4dd2621b3b3d69e
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-26 17:10:33 -0400

    - Tested InstallerHelper

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Net;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class InstallerHelperTests
+    {
+        private Mock<IFileDownLoader> _fileDownLoader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownLoader = new Mock<IFileDownLoader>();
+            _installerHelper = new InstallerHelper(_fileDownLoader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownLoader.Setup(fd => 
+                fd.DownLoadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result =_installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
